c13686d82ba4584183575589bc74696d41f28687
Thulani <tolanym_2@live.com>
2025-01-14 13:44:24 +0200

    initial commit

---
+++ src/main/java/com/billio/billio_be/BillioBeApplication.java
@@ -0,0 +1,14 @@
+package com.billio.billio_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillioBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillioBeApplication.class, args);
+		System.out.println("Billio BE up and running");
+	}
+
+}
---
+++ src/test/java/com/billio/billio_be/BillioBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billio.billio_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillioBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
